b142efcf85128d5613ff95cb1d64f4d101f9170b
techiemow <mowriyaabk@gmail.com>
2024-07-19 12:31:23 +0530

    initial api setup done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const connectdb = require("./DB");
 
 
 app.use(cors());
 app.use(bodyParser.json());
+connectdb();
+
+app.get("/", function(req, res) {
+    res.send("Welcome to the BusVoyage Page!");
+})
+
 
 
 app.listen( 5000, () => {
